fb29091fc18b373e96da65d7a9c39defb9f48163
Cristhian Ortiz <cristhian.ortiz.kommit@gmail.com>
2022-10-14 16:44:30 -0500

    Se realizaron todas las modificaciones de la guía del módulo 2

--- hello/lib/hello_web/views/page_view.ex
+++ hello/lib/hello_web/views/page_view.ex
@@ -1,3 +1,14 @@
 defmodule HelloWeb.PageView do
   use HelloWeb, :view
+
+  def render("index.json", %{pages: pages}) do
+    %{data: Enum.map(pages, fn page -> %{title: page.title} end)}
+  end
+
+  def render("show.json", %{page: page}) do
+    %{data: %{title: page.title}}
+  end
+  # def render("index.html", assigns) do
+  #   "rendering with assigns #{inspect Map.keys(assigns)}"
+  # end
 end
